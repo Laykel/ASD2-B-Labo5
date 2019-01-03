eb3f8b881b646232fb8b2817eb33c98aa6b4a7fa
Laykel <luc.wachter@gmail.com>
2019-01-03 15:29:56 +0100

    Template pour main

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+/*
+ * File: main.cpp
+ * ASD 2 - Labo 5 - Mini projet
+ * Authors: Loris Gilliand, Mateo Tutic, Luc Wachter
+ * Created on 03.01.2019
+ *
+ * Description: Fichier d'entrée du programme, permet de tester le correcteur
+ *              orthographique avec les deux structures de données proposées à
+ *              l'aide de la constante DATA_STRUCTURE.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+/* #include "BinarySearchTree.h" */
+#include "AVLTree.h"
+
+// Constante définissant quelle structure de données utiliser
+#define DATA_STRUCTURE "SET"
+
+using namespace std;
+
+void test(string dictionaryFile, string textFile) {
+   // Lire dictionary et le stocker (FileIO.h, Dictionary.h)
+
+   // Lire texte et comparer chaque mot avec dictionnaire (FileIO.h, main.cpp)
+
+      // Si le mot est dans le dictionnaire, ne rien faire
+      // Sinon, stocker le mot dans une liste de mots incorrects (SpellCheck.h)
+
+   // Parcourir la liste de mots incorrects et effectuer les 4 modifications
+   // pour chaque caractère, comparer le mot modifié avec dictionnaire et stocker
+   // dans fichier toutes les corrections possibles (SpellCheck.h, FileIO.h)
+}
+
+int main() {
+   { // Tester AVLTree
+      AVLTree<string, string> a;
+      a.put("name", "Valentine");
+      a.put("power", "flight");
+      a.put("color", "blue");
+
+      string value;
+      a.get("name", value);
+      cout << value << endl;
+   }
+
+   return EXIT_SUCCESS;
+}
