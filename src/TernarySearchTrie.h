3aea117b8a426d82ecb42b6e822b90259dcad434
Laykel <luc.wachter@gmail.com>
2019-01-03 15:29:14 +0100

    Template pour la classe TernarySearchTrie

---
+++ src/TernarySearchTrie.h
@@ -0,0 +1,18 @@
+/*
+ * File: TernarySearchTrie.h
+ * ASD 2 - Labo 5 - Mini projet
+ * Authors: Loris Gilliand, Mateo Tutic, Luc Wachter
+ * Created on 03.01.2019
+ *
+ * Description: Classe implémentant les tries ternaires de recherche.
+ */
+
+#ifndef TERNARYSEARCHTRIE_H
+#define TERNARYSEARCHTRIE_H
+
+template <typename type>
+class TernarySearchTrie {
+
+};
+
+#endif
