729d0af32bcf4ea9ba2c58a2f71bc5acaa19a0ae
Laykel <luc.wachter@gmail.com>
2019-01-07 20:42:18 +0100

    Utilisation de listes pour misspelled, écriture dans le fichier de sortie

--- src/SpellCheck.h
+++ src/SpellCheck.h
@@ -31,30 +31,15 @@ public:
     * @param dictionaryFile Le chemin vers le fichier dictionnaire
     * @param textFile Le chemin vers le fichier de texte
     */
-   SpellCheck(std::string dictionaryFile, std::string textFile) {
+   SpellCheck(std::string dictionaryFile, std::string textFile, std::string outputFile) {
+      // On construit le dictionnaire et on l'affecte par copie
       dict = Dictionary<Container>(dictionaryFile);
 
+      // On enregistre tous les mots mal orthographiés
       findMisspelledWords(textFile);
 
-      for (std::string word : misspelled) {
-         // On écrit le mot dans le fichier de sortie
-         // On génère toutes les variantes
-         generateAllVariants(word);
-         // On écrit les variantes dans le fichier de sortie
-      }
-   }
-
-   /**
-    *
-    */
-   std::vector<std::string> generateAllVariants(std::string) {
-      return {"", "2"};
-   }
-
-   // TOREMOVE
-   void printMisspelled() {
-      for (typename std::vector<std::string>::iterator it = misspelled.begin(); it != misspelled.end(); ++it)
-         std::cout << *it << ", ";
+      // On écrit les mots et toutes leurs variantes dans le fichier de sortie
+      writeToFile(outputFile);
    }
 
 private:
@@ -64,40 +49,112 @@ private:
     *
     * @param textFile Le chemin vers le fichier de texte
     */
-   void findMisspelledWords(std::string textFile) {
-      // File stream sur le fichier de texte
-      std::ifstream df(textFile);
+   void findMisspelledWords(std::string filename) {
+      // On lit le fichier de texte et on insère chaque mot ne se trouvant pas
+      // dans le dictionnaire dans la liste des mots mal orthographiés
+      readFile(filename, [this](std::string word) {
+                  // Si le mot ne se trouve pas dans le dictionnaire
+                  if (!dict.contains(word)) {
+                     // On insère le mot dans la liste de mots mal orthographiés
+                     misspelled.push_back(word);
+                  }
+               });
+   }
 
-      // On vérifie si le file stream est bien lié à un fichier ouvert
-      if (df.is_open()) {
-         std::string word;
+   /**
+    * Génère toutes les variantes du mot donné selon les quatre règles et les
+    * renvoie préfixées de leur numéro de règle
+    *
+    * @param word Le mot dont les variantes doivent être générées
+    * @return un vecteur contenant toutes les variantes
+    */
+   std::list<std::string> generateAllVariants(std::string word) {
+      const std::string characters = "abcdefghijklmnopqrstuvwxyz'";
+      std::string variant;
+      std::list<std::string> variants;
 
-         // Tant que des mots nous sont retournés
-         while (df >> word) {
-            // On nettoie le mot
-            word = sanitize(word);
+      // 1. L'utilisateur a tapé une lettre supplémentaire
+      for (size_t i = 0; i < word.length(); ++i) {
+         variant = word;
+         variant.erase(i, 1);
 
-            // On s'assure que le mot n'est pas vide
-            if (!word.empty()) {
-               // Si le mot ne se trouve pas dans le dictionnaire
-               if (!dict.contains(word)) {
-                  // On insère le mot dans la liste de mots mal orthographiés
-                  misspelled.push_back(word);
-               }
+         if (dict.contains(variant))
+            variants.push_back("1:" + variant);
+      }
+
+      // 2. L'utilisateur a oublié de taper une lettre
+      for (size_t i = 0; i < word.length(); ++i) {
+         for(char c : characters) {
+            variant = word;
+
+            variant.insert(i, 1, c);
+
+            if (dict.contains(variant))
+               variants.push_back("2:" + variant);
+         }
+      }
+
+      // 3. L'utilisateur a mal tapé une lettre
+      for (size_t i = 0; i < word.length(); ++i) {
+         variant = word;
+
+         for(char c : characters) {
+            variant.at(i) = c;
+
+            if (dict.contains(variant))
+               variants.push_back("3:" + variant);
+         }
+      }
+
+      // 4. L'utilisateur a échangé deux lettres consécutives
+      for (size_t i = 0; i < word.length() - 1; ++i) {
+         variant = word;
+
+         variant.at(i) = variant.at(i + 1);
+         variant.at(i + 1) = word.at(i);
+
+         if (dict.contains(variant))
+            variants.push_back("4:" + variant);
+      }
+
+      return variants;
+   }
+
+   /**
+    * Écrit les mots mal orthographiés et toutes leurs variantes correctes dans
+    * le fichier donné en paramètre
+    *
+    * @param filename Le chemin vers le fichier de sortie
+    */
+   void writeToFile(std::string filename) {
+      std::ofstream output(filename);
+
+      if (output.is_open()) {
+         std::list<std::string> variants; // Vecteur de variantes orthographiques
+         // Pour chaque mot dans la liste de mots mal orthographiés
+         for (std::string word : misspelled) {
+            // On écrit le mot dans le fichier de sortie
+            output << "*" << word << std::endl;
+
+            // On génère toutes les variantes orthographiques de ce mot
+            variants = generateAllVariants(word);
+
+            // On écrit les variantes dans le fichier de sortie
+            for (std::string variant : variants) {
+               output << variant << std::endl;
             }
          }
-
-         // Fermeture du file stream
-         df.close();
-      } else {
-         // TODO exception or something else ?
-         std::cout << "Nom du fichier incorrect" << std::endl;
       }
+      else {
+         // TODO Handle error
+      }
+
+      output.close();
    }
 
 private:
    Dictionary<Container> dict;
-   std::vector<std::string> misspelled;
+   std::list<std::string> misspelled;
 };
 
 #endif
