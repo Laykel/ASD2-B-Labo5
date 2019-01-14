cff69f8cf28bb317dc2c82303df4cc000bca1dc7
Laykel <luc.wachter@gmail.com>
2019-01-14 20:10:10 +0100

    Séparation des 4 fonctions de variantes, améliorations

--- src/SpellCheck.h
+++ src/SpellCheck.h
@@ -29,91 +29,129 @@ public:
     * trouve les mots mal orthographiés, génère les variantes et écrit le fichier
     * de sortie
     *
-    * @param dictionaryFile Le chemin vers le fichier dictionnaire
+    * @param dict Une référence constante vers le dictionnaire
     * @param textFile Le chemin vers le fichier de texte
     */
-   SpellCheck(const Dictionary<Container>& dict, std::string textFile, std::string outputFile)
+   SpellCheck(const Dictionary<Container>& dict, std::string textFile)
    : dict(dict) {
       // On enregistre tous les mots mal orthographiés
       findMisspelledWords(textFile);
-
-      // On écrit les mots et toutes leurs variantes dans le fichier de sortie
-      writeToFile(outputFile);
    }
 
-private:
    /**
-    * Lit le fichier de texte mot par mot et enregistre chaque mot non présent
-    * dans le dictionnaire dans la liste misspelled
+    * Getter : permet de récupérer la liste de mots mal orthographiés
     *
-    * @param textFile Le chemin vers le fichier de texte
+    * @return la liste de mots mal orthographiés
     */
-   void findMisspelledWords(std::string filename) {
-      // On lit le fichier de texte et on insère chaque mot ne se trouvant pas
-      // dans le dictionnaire dans la liste des mots mal orthographiés
-      readFile(filename, [this](std::string word) {
-                  // Si le mot ne se trouve pas dans le dictionnaire
-                  if (!dict.contains(word)) {
-                     // On insère le mot dans la liste de mots mal orthographiés
-                     misspelled.push_back(word);
-                  }
-               });
+   std::list<std::string> getMisspelledWords() {
+      return misspelled;
    }
 
    /**
-    * Génère toutes les variantes du mot donné selon les quatre règles et les
-    * renvoie préfixées de leur numéro de règle
+    * Variante orthographique 1 : L'utilisateur a tapé une lettre supplémentaire
     *
     * @param word Le mot dont les variantes doivent être générées
-    * @return un vecteur contenant toutes les variantes
+    * @return la liste des variantes correctes du mot
     */
-   std::list<std::string> generateAllVariants(std::string word) {
-      const std::string characters = "abcdefghijklmnopqrstuvwxyz'";
+   std::list<std::string> variant1(std::string word) {
       std::string variant;
       std::list<std::string> variants;
 
-      // 1. L'utilisateur a tapé une lettre supplémentaire
+      // Pour chaque caractère du mot
       for (size_t i = 0; i < word.length(); ++i) {
          variant = word;
+         // On efface ce caractère
          variant.erase(i, 1);
 
+         // Et si le mot résultant est dans le dictionnaire
          if (dict.contains(variant))
-            variants.push_back("1:" + variant);
+            // On l'ajoute aux variantes correctes
+            variants.push_back(variant);
       }
 
-      // 2. L'utilisateur a oublié de taper une lettre
+      return variants;
+   }
+
+   /**
+    * Variante orthographique 2 : L'utilisateur a oublié de taper une lettre
+    *
+    * @param word Le mot dont les variantes doivent être générées
+    * @return la liste des variantes correctes du mot
+    */
+   std::list<std::string> variant2(std::string word) {
+      std::string variant;
+      std::list<std::string> variants;
+
+      // Pour chaque position dans le mot
       for (size_t i = 0; i < word.length(); ++i) {
-         for(char c : characters) {
+         // Pour chaque caractère autorisé
+         for(char c : allowedCharacters) {
             variant = word;
 
+            // On insère ce caractère à la position courante
             variant.insert(i, 1, c);
 
+            // Et si le mot résultant est dans le dictionnaire
             if (dict.contains(variant))
-               variants.push_back("2:" + variant);
+               // On l'ajoute aux variantes correctes
+               variants.push_back(variant);
          }
       }
 
-      // 3. L'utilisateur a mal tapé une lettre
+      return variants;
+   }
+
+   /**
+    * Variante orthographique 3 : L'utilisateur a mal tapé une lettre
+    *
+    * @param word Le mot dont les variantes doivent être générées
+    * @return la liste des variantes correctes du mot
+    */
+   std::list<std::string> variant3(std::string word) {
+      std::string variant;
+      std::list<std::string> variants;
+
+      // Pour chaque caractère du mot
       for (size_t i = 0; i < word.length(); ++i) {
          variant = word;
 
-         for(char c : characters) {
+         // Pour chaque caractère autorisé
+         for(char c : allowedCharacters) {
+            // On remplace le caractère à la position par ce caractère
             variant.at(i) = c;
 
+            // Et si le mot résultant est dans le dictionnaire
             if (dict.contains(variant))
-               variants.push_back("3:" + variant);
+               // On l'ajoute aux variantes correctes
+               variants.push_back(variant);
          }
       }
 
-      // 4. L'utilisateur a échangé deux lettres consécutives
+      return variants;
+   }
+
+   /**
+    * Variante orthographique 4 : L'utilisateur a échangé deux lettres consécutives
+    *
+    * @param word Le mot dont les variantes doivent être générées
+    * @return la liste des variantes correctes du mot
+    */
+   std::list<std::string> variant4(std::string word) {
+      std::string variant;
+      std::list<std::string> variants;
+
+      // Pour chaque position dans le mot
       for (size_t i = 0; i < word.length() - 1; ++i) {
          variant = word;
 
+         // On échange le caractère à la position courante avec le suivant
          variant.at(i) = variant.at(i + 1);
          variant.at(i + 1) = word.at(i);
 
+         // Et si le mot résultant est dans le dictionnaire
          if (dict.contains(variant))
-            variants.push_back("4:" + variant);
+            // On l'ajoute aux variantes correctes
+            variants.push_back(variant);
       }
 
       return variants;
@@ -155,8 +193,59 @@ private:
    }
 
 private:
+   /**
+    * Lit le fichier de texte mot par mot et enregistre chaque mot non présent
+    * dans le dictionnaire dans la liste misspelled
+    *
+    * @param textFile Le chemin vers le fichier de texte
+    */
+   void findMisspelledWords(std::string filename) {
+      // On lit le fichier de texte et on insère chaque mot ne se trouvant pas
+      // dans le dictionnaire dans la liste des mots mal orthographiés
+      readFile(filename, [this](std::string word) {
+                  // Si le mot ne se trouve pas dans le dictionnaire
+                  if (!dict.contains(word)) {
+                     // On insère le mot dans la liste de mots mal orthographiés
+                     misspelled.push_back(word);
+                  }
+               });
+   }
+
+   /**
+    * Génère toutes les variantes du mot donné selon les quatre règles et les
+    * renvoie préfixées de leur numéro de règle
+    *
+    * @param word Le mot dont les variantes doivent être générées
+    * @return un vecteur contenant toutes les variantes préfixées de leur numéro
+    */
+   std::list<std::string> generateAllVariants(std::string word) {
+      std::list<std::string> variants;
+
+      // On itère sur le nombre de fonction génératrices de variantes
+      for (int i = 0; i < NBR_VARIANT_FUNCTIONS; ++i) {
+         // Pour chaque variante générée par la fonction
+         for (std::string variant : variantFunc[i](word)) {
+            // On ajoute la variante avec son numéro de variante dans
+            // la liste de variantes
+            variants.push_back(std::to_string(i + 1) + ":" + variant);
+         }
+      }
+
+      return variants;
+   }
+
+private:
    const Dictionary<Container>& dict;
    std::list<std::string> misspelled;
+   const std::string allowedCharacters = "abcdefghijklmnopqrstuvwxyz'";
+
+   // Tableau de fonctions génératrices de variantes orthographiques
+   std::function<std::list<std::string>(std::string)> variantFunc[NBR_VARIANT_FUNCTIONS] = {
+      std::bind(&SpellCheck::variant1, this, std::placeholders::_1),
+      std::bind(&SpellCheck::variant2, this, std::placeholders::_1),
+      std::bind(&SpellCheck::variant3, this, std::placeholders::_1),
+      std::bind(&SpellCheck::variant4, this, std::placeholders::_1)
+   };
 };
 
 #endif
